dd5c2fa7a9b2f77c41d48ce5e20dca3ee47bfa5e
Gustavo Esteban Borragán <gustavo.esteban.borragan@gmail.com>
2020-06-12 23:07:37 +0200

    Iniciando

---
+++ php/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+
+<html lang="es">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>ASIR - Proyecto integrado</title>
+  <link href="bootstrap.min.css" rel="stylesheet">
+  <link href="style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <?php
+    $nota = "?";
+  ?>
+
+  <header class="bg-success text-white">
+    <div class="text-center">
+      <h2>Mi nota es un...</h2>
+      <h1><?php echo $nota ?></h1>
+    </div>
+  </header>
+
+</body>
+
+<script>
+ // setTimeout(() => { window.location = "/" }, 1000 * 5)
+</script>
+
+</html>
